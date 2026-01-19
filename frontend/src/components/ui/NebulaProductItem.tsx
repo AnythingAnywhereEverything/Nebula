130642f54da3418028089382b2ecd02fda067e32
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-01-19 22:17:14 +0700

    chore(frontend): move the product item to UI folder

--- frontend/src/components/ui/NebulaProductItem.tsx
+++ frontend/src/components/ui/NebulaProductItem.tsx
@@ -1,13 +1,15 @@
 
-import React, { FC } from 'react';
-import { NerdFonts } from '../../utilities/NerdFonts';
-import style from '@styles/features/searchresult.module.scss';
+import React from 'react';
+import { NerdFonts } from '../utilities/NerdFonts';
 import { ratingStars } from '@components/utilities/StarRating';
+import style from '@styles/features/searchresult.module.scss';
 
 type SearchResultItemProps = {
     itemid: string;
+
     itemtag?: string;
     itemtagcolor?: string;
+
     itemtitle: string;
     itemimageurl: string;
     itemprice_usd: number;
@@ -25,10 +27,19 @@ const SearchResultItem: React.FC<SearchResultItemProps> = ({ itemid, itemtag, it
                 <img src={itemimageurl} alt="Item" className={style.itemImage} />
             </div>
             <div className={style.itemDetails}>
-                <a className={style.itemTitle} title={itemtitle} href={`/product/${encodeURIComponent(itemtitle)}/q/${itemid_encoded}`}>{itemtitle}</a>
-                <p className={style.itemPrice}>${itemprice_usd}</p>
-                <div className={style.itemRating}>
-                    <NerdFonts>{ratingStars(itemrating)}</NerdFonts> {itemrating}
+                <div className={style.topContent}>
+                    <a className={style.itemTitle} 
+                        title={itemtitle}
+                        href={`/product/${encodeURIComponent(itemtitle)}/q/${itemid_encoded}`}>{itemtitle}</a>
+                </div>
+                <div className={style.bottomContent}>
+                    <p className={style.itemPrice}>${itemprice_usd}</p>
+                    <p className={style.itemRating}>
+                        <NerdFonts>{ratingStars(itemrating)}</NerdFonts> {itemrating}
+                    </p>
+                    <p>
+                        <NerdFonts></NerdFonts> {productLocation}
+                    </p>
                 </div>
             </div>
         </div>
