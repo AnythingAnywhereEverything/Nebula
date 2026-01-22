6bc92fbdbc97c37844d6d8e4a5878121aa1fa8c1
seinaru <krittitee.petch@gmail.com>
2026-01-22 21:59:38 +0700

    feat: history page
    - User can unsee the product they don't want for Algorithm (If we can create it)
    - add historyItem in seachResultItem in scss due to using same component

---
+++ frontend/src/pages/browsingHistory.tsx
@@ -0,0 +1,21 @@
+import { NerdFonts } from "@components/utilities/NerdFonts";
+import React from "react";
+import style from "@styles/layouts/browsingHistory.module.scss";
+import HistoryContainer from "@components/layouts/historyContainer";
+import BottomProductContent from "@components/layouts/bottomProductContent";
+
+export default function BrowsingHistory() {
+    return(
+        <section className={style.historySection}>
+            <h2 className={style.historyHeader}>
+                <NerdFonts>Your Browsing History</NerdFonts>
+            </h2>
+
+            <div>
+                <HistoryContainer/>                
+            </div>
+
+            <BottomProductContent/>
+        </section>
+    )
+}
