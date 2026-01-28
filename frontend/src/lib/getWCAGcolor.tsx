3a4322c2c1acec5e115c35d571a2d306add19d17
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-01-28 21:33:13 +0700

    feat(utils): add new utilities

---
+++ frontend/src/lib/getWCAGcolor.tsx
@@ -0,0 +1,10 @@
+export function getWCAGColor(hex: string) {
+    const c = hex.replace("#", "");
+    const r = parseInt(c.slice(0, 2), 16) / 255;
+    const g = parseInt(c.slice(2, 4), 16) / 255;
+    const b = parseInt(c.slice(4, 6), 16) / 255;
+
+    const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+    return luminance > 0.6 ? "#000" : "#fff";
+}
