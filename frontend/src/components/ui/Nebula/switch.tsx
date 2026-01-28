3d2a6d3efd9403b994bf1cedde789a12b139becb
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-01-29 02:20:33 +0700

    feat(ui): add switch ui

---
+++ frontend/src/components/ui/Nebula/switch.tsx
@@ -0,0 +1,30 @@
+import { Switch as SwitchPrimitive } from "radix-ui"
+import { cn } from "src/lib/utils"
+
+import s from "@styles/ui/Nebula/switch.module.scss"
+
+function Switch({
+  className,
+  size = "default",
+  ...props
+}: React.ComponentProps<typeof SwitchPrimitive.Root> & {
+  size?: "sm" | "default"
+}) {
+  return (
+    <SwitchPrimitive.Root
+      data-slot="switch"
+      data-size={size}
+      className={cn(
+        s.switch,
+        className
+      )}
+      {...props}
+    >
+      <SwitchPrimitive.Thumb
+        data-slot="switch-thumb"
+        className={s.switchThumb}
+      />
+    </SwitchPrimitive.Root>
+  )
+}
+export { Switch }
