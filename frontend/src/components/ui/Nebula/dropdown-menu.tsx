5c7eb894ea5b946117655ec46af4cae8fead22a4
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-01-29 20:36:43 +0700

    feat(ui): add dropdown UI and update the test page

---
+++ frontend/src/components/ui/Nebula/dropdown-menu.tsx
@@ -0,0 +1,244 @@
+
+import { DropdownMenu as DropdownMenuPrimitive } from "radix-ui"
+import { cn } from "src/lib/utils"
+import { Icon } from "../NebulaUI"
+
+import s from "@styles/ui/Nebula/dropdownmenu.module.scss"
+
+function DropdownMenu({
+    ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.Root>) {
+    return <DropdownMenuPrimitive.Root data-component="dropdown-menu" {...props} />
+}
+
+function DropdownMenuPortal({
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.Portal>) {
+  return (
+    <DropdownMenuPrimitive.Portal data-component="dropdown-menu-portal" {...props} />
+  )
+}
+
+function DropdownMenuTrigger({
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.Trigger>) {
+  return (
+    <DropdownMenuPrimitive.Trigger
+      data-component="dropdown-menu-trigger"
+      {...props}
+    />
+  )
+}
+
+function DropdownMenuContent({
+  className,
+  align = "start",
+  sideOffset = 4,
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.Content>) {
+  return (
+    <DropdownMenuPrimitive.Portal>
+      <DropdownMenuPrimitive.Content
+        data-component="dropdown-menu-content"
+        sideOffset={sideOffset}
+        align={align}
+        className={cn(s.menuContent, className )}
+        {...props}
+      />
+    </DropdownMenuPrimitive.Portal>
+  )
+}
+
+function DropdownMenuGroup({
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.Group>) {
+  return (
+    <DropdownMenuPrimitive.Group data-component="dropdown-menu-group" {...props} />
+  )
+}
+
+
+function DropdownMenuItem({
+  className,
+  inset,
+  variant = "default",
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.Item> & {
+  inset?: boolean
+  variant?: "default" | "destructive"
+}) {
+  return (
+    <DropdownMenuPrimitive.Item
+      data-component="dropdown-menu-item"
+      data-inset={inset}
+      data-variant={variant}
+      className={cn(
+        s.dropdownItem,
+        className
+      )}
+      {...props}
+    />
+  )
+}
+function DropdownMenuCheckboxItem({
+  className,
+  children,
+  checked,
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.CheckboxItem>) {
+  return (
+    <DropdownMenuPrimitive.CheckboxItem
+      data-component="dropdown-menu-checkbox-item"
+      className={cn(
+        s.dropdownItem,
+        className
+      )}
+      checked={checked}
+      {...props}
+    >
+      <span
+        className={s.indicator}
+        data-component="dropdown-menu-checkbox-item-indicator"
+      >
+        <DropdownMenuPrimitive.ItemIndicator>
+          <Icon value=""
+          />
+        </DropdownMenuPrimitive.ItemIndicator>
+      </span>
+      {children}
+    </DropdownMenuPrimitive.CheckboxItem>
+  )
+}
+function DropdownMenuRadioGroup({
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.RadioGroup>) {
+  return (
+    <DropdownMenuPrimitive.RadioGroup
+      data-component="dropdown-menu-radio-group"
+      {...props}
+    />
+  )
+}
+function DropdownMenuRadioItem({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.RadioItem>) {
+  return (
+    <DropdownMenuPrimitive.RadioItem
+      data-component="dropdown-menu-radio-item"
+      className={cn(
+        s.dropdownItem,
+        className
+      )}
+      {...props}
+    >
+      <span
+        className={s.indicator}
+        data-component="dropdown-menu-radio-item-indicator"
+      >
+        <DropdownMenuPrimitive.ItemIndicator>
+          <Icon value=""/>
+        </DropdownMenuPrimitive.ItemIndicator>
+      </span>
+      {children}
+    </DropdownMenuPrimitive.RadioItem>
+  )
+}
+function DropdownMenuLabel({
+  className,
+  inset,
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.Label> & {
+  inset?: boolean
+}) {
+  return (
+    <DropdownMenuPrimitive.Label
+      data-component="dropdown-menu-label"
+      data-inset={inset}
+      className={cn(s.label, className)}
+      {...props}
+    />
+  )
+}
+function DropdownMenuSeparator({
+  className,
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.Separator>) {
+  return (
+    <DropdownMenuPrimitive.Separator
+      data-component="dropdown-menu-separator"
+      className={cn(s.separator, className)}
+      {...props}
+    />
+  )
+}
+function DropdownMenuShortcut({
+  className,
+  ...props
+}: React.ComponentProps<"span">) {
+  return (
+    <span
+      data-component="dropdown-menu-shortcut"
+      className={cn(s.menuShortcut, className)}
+      {...props}
+    />
+  )
+}
+function DropdownMenuSub({
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.Sub>) {
+  return <DropdownMenuPrimitive.Sub data-component="dropdown-menu-sub" {...props} />
+}
+function DropdownMenuSubTrigger({
+  className,
+  inset,
+  children,
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.SubTrigger> & {
+  inset?: boolean
+}) {
+  return (
+    <DropdownMenuPrimitive.SubTrigger
+      data-component="dropdown-menu-sub-trigger"
+      data-inset={inset}
+      className={cn(
+        s.dropdownItem,
+        className
+      )}
+      {...props}
+    >
+      {children}
+      <Icon style={{marginLeft: "auto"}} value="" />
+    </DropdownMenuPrimitive.SubTrigger>
+  )
+}
+function DropdownMenuSubContent({
+  className,
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.SubContent>) {
+  return (
+    <DropdownMenuPrimitive.SubContent
+      data-component="dropdown-menu-sub-content"
+      className={cn(s.menuSubContent, className )}
+      {...props}
+    />
+  )
+}
+export {
+  DropdownMenu,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+}
--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,11 +1,14 @@
 import style from '@styles/home.module.scss';
 import { NextPageWithLayout } from '../types/global.d';
-import { DropdownMenuTrigger, DropdownMenuContent, DropdownMenuGroup, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator, DropdownMenu } from '@components/ui/Nebula/dropdown-menu';
+import { DropdownMenuTrigger, DropdownMenuContent, DropdownMenuGroup, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator, DropdownMenu, DropdownMenuPortal, DropdownMenuShortcut, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger } from '@components/ui/Nebula/dropdown-menu';
 import Link from 'next/link';
-import {Dialog, Button, Input, Textarea, InputGroup, InputGroupInput, InputGroupAddon, Icon, InputGroupText, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose, ButtonGroup, ButtonGroupSeparator, ProductItem, ProductImage, ProductContent, ProductHeader, ProductFooter, ProductStars, ProductLocation, ProductPrice } from '@components/ui/NebulaUI';
+import {Dialog, Button, Input, Textarea, InputGroup, InputGroupInput, InputGroupAddon, Icon, InputGroupText, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose, ButtonGroup, ButtonGroupSeparator, ProductItem, ProductImage, ProductContent, ProductHeader, ProductFooter, ProductStars, ProductLocation, ProductPrice, InputGroupButton, Label } from '@components/ui/NebulaUI';
 import { Badge } from '@components/ui/Nebula/badge';
 import { ProductContainer, ProductContainerDescription, ProductContainerHeader, ProductContainerHeaderAddon, ProductContainerTitle, ProductField } from '@components/ui/Nebula/product-field';
-import { SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem, Select } from '@components/ui/Nebula/selector';
+import { SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem, Select, SelectSeparator } from '@components/ui/Nebula/selector';
+import { Switch } from '@components/ui/Nebula/switch';
+import { Field, FieldLabel, FieldDescription, FieldGroup, FieldLegend, FieldSet, FieldContent, FieldTitle } from '@components/ui/Nebula/field';
+import { Checkbox } from '@components/ui/Nebula/checkbox';
 
 const TestPage: NextPageWithLayout = () => {
 
@@ -19,6 +22,44 @@ const TestPage: NextPageWithLayout = () => {
         <Button variant={"outline"} size={"lg"}>TEST lg</Button>
         <Button variant={"destructive"}>TEST destructive</Button>
         <Input placeholder="Hello"/>
+
+        <FieldGroup style={{width:"100%", maxWidth:"25rem"}}>
+            <Field orientation="horizontal">
+                <Checkbox id="terms-checkbox" name="terms-checkbox" />
+                <Label htmlFor="terms-checkbox">Accept terms and conditions</Label>
+            </Field>
+            <Field orientation="horizontal">
+                <Checkbox
+                id="terms-checkbox-2"
+                name="terms-checkbox-2"
+                defaultChecked
+                />
+                <FieldContent>
+                <FieldLabel htmlFor="terms-checkbox-2">
+                    Accept terms and conditions
+                </FieldLabel>
+                <FieldDescription>
+                    By clicking this checkbox, you agree to the terms.
+                </FieldDescription>
+                </FieldContent>
+            </Field>
+            <Field orientation="horizontal" data-disabled>
+                <Checkbox id="toggle-checkbox" name="toggle-checkbox" disabled />
+                <FieldLabel htmlFor="toggle-checkbox">Enable notifications</FieldLabel>
+            </Field>
+            <FieldLabel>
+                <Field orientation="horizontal">
+                    <Checkbox id="toggle-checkbox-2" name="toggle-checkbox-2" />
+                    <FieldContent>
+                        <FieldTitle>Enable notifications</FieldTitle>
+                        <FieldDescription>
+                        You can enable or disable notifications at any time.
+                        </FieldDescription>
+                    </FieldContent>
+                </Field>
+            </FieldLabel>
+            </FieldGroup>
+
         <Textarea placeholder="test message"/>
         <InputGroup>
             <InputGroupInput id="inline-start-input" placeholder="Search..." />
@@ -32,6 +73,82 @@ const TestPage: NextPageWithLayout = () => {
                 <InputGroupText>USD</InputGroupText>
             </InputGroupAddon>
         </InputGroup>
+
+
+
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+            <Button variant="outline">Open</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+            <DropdownMenuGroup>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+            </DropdownMenuGroup>
+                <DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Team</DropdownMenuItem>
+                    <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+
+
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline">Open</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent style={{width:"calc(var(--spacing)*40)"}} align="start">
+                <DropdownMenuGroup>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuItem>
+                    Profile
+                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    Billing
+                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    Settings
+                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                    <DropdownMenuSubContent>
+                        <DropdownMenuItem>Email</DropdownMenuItem>
+                        <DropdownMenuItem>Message</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>More...</DropdownMenuItem>
+                    </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                </DropdownMenuSub>
+                <DropdownMenuItem>
+                    New Team
+                    <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                <DropdownMenuItem>GitHub</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuItem disabled>API</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                <DropdownMenuItem>
+                    Log out
+                    <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+            </DropdownMenu>
 
         <Dialog>
             <DialogTrigger asChild>
@@ -67,12 +184,17 @@ const TestPage: NextPageWithLayout = () => {
         <ButtonGroup>
             <ButtonGroup>
                 <Button variant="outline" size="icon">
-                <Icon value='+' />
+                    <Icon value='+' />
                 </Button>
             </ButtonGroup>
             <ButtonGroup>
                 <InputGroup>
-                <InputGroupInput placeholder="Send a message..." />
+                    <InputGroupAddon align="inline-end">
+                        <InputGroupButton variant="ghost" size="icon-xs">
+                            <Icon value='+' />
+                        </InputGroupButton>
+                    </InputGroupAddon>
+                    <InputGroupInput placeholder="Send a message..." />
                 </InputGroup>
             </ButtonGroup>
         </ButtonGroup>
@@ -86,12 +208,12 @@ const TestPage: NextPageWithLayout = () => {
                 Paste
             </Button>
         </ButtonGroup>
-{/* 
+
         <Select>
             <SelectTrigger style={{width:"100%", maxWidth:"calc(var(--spacing)*48)"}}>
                 <SelectValue placeholder="Select a fruit" />
             </SelectTrigger>
-            <SelectContent>
+            <SelectContent position="popper">
                 <SelectGroup>
                     <SelectLabel>Fruits</SelectLabel>
                     <SelectItem value="apple">Apple</SelectItem>
@@ -101,7 +223,28 @@ const TestPage: NextPageWithLayout = () => {
                     <SelectItem value="pineapple">Pineapple</SelectItem>
                 </SelectGroup>
             </SelectContent>
-        </Select> */}
+        </Select>
+
+        <Select>
+            <SelectTrigger className="w-full max-w-48">
+                <SelectValue placeholder="Select a fruit" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectLabel>Fruits</SelectLabel>
+                    <SelectItem value="apple">Apple</SelectItem>
+                    <SelectItem value="banana">Banana</SelectItem>
+                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                </SelectGroup>
+                <SelectSeparator />
+                <SelectGroup>
+                    <SelectLabel>Vegetables</SelectLabel>
+                    <SelectItem value="carrot">Carrot</SelectItem>
+                    <SelectItem value="broccoli">Broccoli</SelectItem>
+                    <SelectItem value="spinach">Spinach</SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
 
         <ProductItem orientation={"horizontal"}>
             <ProductImage src="https://placehold.co/400x800"/>
@@ -120,6 +263,55 @@ const TestPage: NextPageWithLayout = () => {
                 </ProductFooter>
             </ProductContent>
         </ProductItem>
+
+        <Switch />
+
+            <Field style={{width:"100%", maxWidth:"calc(var(--spacing)*48)"}}>
+                <FieldLabel>Department</FieldLabel>
+                    <Select>
+                        <SelectTrigger>
+                        <SelectValue placeholder="Choose department" />
+                        </SelectTrigger>
+                        <SelectContent>
+                        <SelectGroup>
+                            <SelectItem value="engineering">Engineering</SelectItem>
+                            <SelectItem value="design">Design</SelectItem>
+                            <SelectItem value="marketing">Marketing</SelectItem>
+                            <SelectItem value="sales">Sales</SelectItem>
+                            <SelectItem value="support">Customer Support</SelectItem>
+                            <SelectItem value="hr">Human Resources</SelectItem>
+                            <SelectItem value="finance">Finance</SelectItem>
+                            <SelectItem value="operations">Operations</SelectItem>
+                        </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                <FieldDescription>
+                    Select your department or area of work.
+                </FieldDescription>
+            </Field>
+
+            <FieldSet style={{width:"100%", maxWidth:"20rem"}}>
+                <FieldLegend>Address Information</FieldLegend>
+                <FieldDescription>
+                    We need your address to deliver your order.
+                </FieldDescription>
+                <FieldGroup>
+                    <Field>
+                        <FieldLabel htmlFor="street">Street Address</FieldLabel>
+                        <Input id="street" type="text" placeholder="123 Main St" />
+                    </Field>
+                    <div style={{display: "grid", gridTemplateColumns: "repeat(2, minmax(0, 1fr))", gap: "1rem"}}>
+                        <Field>
+                            <FieldLabel htmlFor="city">City</FieldLabel>
+                            <Input id="city" type="text" placeholder="New York" />
+                        </Field>
+                        <Field>
+                            <FieldLabel htmlFor="zip">Postal Code</FieldLabel>
+                            <Input id="zip" type="text" placeholder="90502" />
+                        </Field>
+                    </div>
+                </FieldGroup>
+            </FieldSet>
 
         <ProductContainer>
             <ProductContainerHeader>
