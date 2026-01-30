c763ebdfe5feb5dd2f6f0eae2007e59cb726d4b3
seinaru <krittitee.petch@gmail.com>
2026-01-30 08:34:13 +0700

    refactor(changePassword): using component instead html element - Change most of all html element to NebulaUi Component - Reveal button fuction - Validation (WIP)

--- frontend/src/components/features/settings/content/changePassword.tsx
+++ frontend/src/components/features/settings/content/changePassword.tsx
@@ -1,47 +1,175 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "@styles/layouts/changePassword.module.scss"
 import { NebulaButton } from "@components/ui/NebulaBtn";
 import Link from "next/link";
+import { Button, ButtonGroup, Field, FieldDescription, FieldGroup , FieldLabel, FieldSet, Icon, Input, InputGroup, InputGroupAddon, InputGroupInput } from "@components/ui/NebulaUI";
+import Form from "next/form";
 
 // Verify first
 const ChangePassword: React.FC = () =>{
+    const [revealPassword, setRevealPassword] = useState(false);
+    const [revealNewPassword, setRevealNewPassword] = useState(false);
+    const [revealConfirm, setRevealConfirm] = useState(false);
+    
+    type Errors = Partial<{
+        currentPassword: string;
+        newPassword: string;
+        confirmPassword: string;
+    }>;
+    
+    const [values, setValues] = useState({
+        currentPassword: "",
+        newPassword: "",
+        confirmPassword: "",
+    });
+
+    const [errors, setErrors] = useState<Errors>({});
+
+        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        setValues(prev => ({
+            ...prev,
+            [name]: value,
+        }));
+
+        if (errors[name as keyof Errors]) {
+            setErrors(prev => ({ ...prev, [name]: undefined }));
+        }
+    };
+
+    const validate = () =>{
+        const nextErrors: Errors = {};
+        
+        // TODO: regEx and validate the password
+
+    }
+        
     return (
         <section className={style.changePasswordContainer}>
-            <form action="">
+            <Form action="#">
+                <FieldSet
+                style={{width: "50%", margin: "4px"}}
+                >
+                    <FieldGroup>
 
-                <div className={style.passwordInput}>
-                    <h3>Current Password</h3>
-                    <div className={style.forgetPassword}>
-                        <Link href={`#`}>forget password?</Link>
-                    </div>
-                    <div>
-                        <input type="password" placeholder="Current password"/>
-                    </div>
-                </div>
+                        <Field>
+                            <FieldLabel htmlFor="currentPassword">Current Password</FieldLabel>
+                                <InputGroup>
+                                
+                                    <InputGroupAddon align="inline-end"
+                                    >
+                                        <Button
+                                        type="button"
+                                            variant="ghost"
+                                            size="icon-xs"
+                                            onClick={() => setRevealPassword(v => !v)}
+                                        >
+                                            <Icon value={revealPassword ? "" : ""} />
+                                        </Button>
+                                    </InputGroupAddon>    
+                                        
+                                    <InputGroupInput
+                                        required
+                                        type={revealPassword ? "text" : "password"}
+                                        name="currentPassword"
+                                        id="current-password"
+                                        value={values.currentPassword}
+                                        onChange={handleChange}
+                                        placeholder="Current password"
+                                    />
 
-                <div className={style.passwordInput}>
-                    <h3>New Password</h3>
-                    <input type="password" placeholder="Current password"/>
-                </div>
+                                </InputGroup>
+                            <FieldDescription>
+                                Forgot Password? <Link href={`#`}>Reset password.</Link>
+                            </FieldDescription>
 
-                <div className={style.passwordInput}>
-                    <h3>Confirm Password</h3>
-                    <input type="password" placeholder="Current password"/>
-                </div>
+                        </Field>
 
-                <div className={style.passwordBtn}>
-                    <NebulaButton 
-                    btnValues ="Reset"
-                    className={style.resetBtn}
-                    onClick={()=>{}}
-                    />
-                    <NebulaButton 
-                    btnValues = "Change Password"
-                    className={style.changePasswordBtn}
-                    onClick={()=>{}}
-                    />
-                </div>
-            </form>
+                        <Field>
+                            <FieldLabel htmlFor="newPassword">New Password</FieldLabel>
+                            <InputGroup>
+                                
+                                <InputGroupAddon align="inline-end">
+                                    <Button
+                                    type="button"
+                                        variant="ghost"
+                                        size="icon-xs"
+                                        onClick={() => setRevealNewPassword(v => !v)}
+                                    >
+                                        <Icon value={revealNewPassword ? "" : ""} />
+                                    </Button>
+                                </InputGroupAddon>    
+                                    
+                                <InputGroupInput
+                                    required
+                                    type={revealNewPassword ? "text" : "password"}
+                                    name="newPassword"
+                                    id="new-password"
+                                    value={values.newPassword}
+                                    onChange={handleChange}
+                                    placeholder="New password"
+                                />
+
+                            </InputGroup>
+                        </Field>
+
+                        <Field>
+                            <FieldLabel htmlFor="confirmPassword">Confirm New Password</FieldLabel>
+                            <InputGroup>
+                                
+                                <InputGroupAddon align="inline-end">
+                                    <Button
+                                    type="button"
+                                        variant="ghost"
+                                        size="icon-xs"
+                                        onClick={() => setRevealConfirm(v => !v)}
+                                    >
+                                        <Icon value={revealConfirm ? "" : ""} />
+                                    </Button>
+                                </InputGroupAddon>    
+                                    
+                                <InputGroupInput
+                                    required
+                                    type={revealConfirm ? "text" : "password"}
+                                    name="confirmPassword"
+                                    id="confirm-password"
+                                    value={values.confirmPassword}
+                                    onChange={handleChange}
+                                    placeholder="Confirm new password"
+                                />
+
+                            </InputGroup>
+                        </Field>
+
+                            <ButtonGroup>
+                                
+                                <ButtonGroup>
+                                    <Button
+                                    variant={`outline`}
+                                    onClick = {() => {console.log("Hello world");
+                                    }}
+                                    >
+                                        Reset
+                                    </Button>
+
+                                </ButtonGroup>
+
+                                <ButtonGroup>
+                                    <Button
+                                    variant={`default`}
+                                    onClick = {() => {console.log("Hello world");
+                                    }}
+                                    >
+                                        Change Password
+                                    </Button>
+                                </ButtonGroup>
+
+                            </ButtonGroup>
+                    </FieldGroup>
+                </FieldSet>
+
+            </Form>
         </section>
     );
 }
