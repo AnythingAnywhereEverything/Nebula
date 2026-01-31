ac3051c4d83096af53ced3901825adcaadad5ca2
seinaru <krittitee.petch@gmail.com>
2026-01-31 20:55:07 +0700

    refactor: using component instead html element
    - remove unused scss ffile

--- frontend/src/components/features/settings/content/orderUpdate.tsx
+++ frontend/src/components/features/settings/content/orderUpdate.tsx
@@ -1,57 +1,35 @@
 import React from "react";
-import style from "@styles/layouts/orderUpdate.module.scss"
-import Link from "next/link";
-import { Icon } from "@components/ui/NebulaUI";
-
+import style from "@styles/layouts/notificationSetting.module.scss"
+import { usePathname } from "next/navigation";
+import NotificationComponent from "../notification/notificationComponent";
+import { orderUpdatesItems } from "src/mocks/notificationComponent.mock";
 
 const OrderUpdate: React.FC = () => {
+    const pathname = usePathname()
+
+    const hasButton = pathname === "/notification/order"
+    const orders = orderUpdatesItems
+    const noOrers = []
     return(
         <section className={style.orderContainer}>
 
-            <div className={style.order}>
-                <div className={style.productImg}>
-                    <img src="https://placehold.co/400" alt="" />
-                </div>
-
-                <div className={style.productInfo}>
-                    <h3>Order Confirm</h3>
-                    
-                    <div className={style.productBottom}>
-                        {/* How to set page to product page */}
-                        <p>
-                            Payment for order <Link href="#">Order number</Link> has been confirmed and 
-                            we've notified the seller. Kindly wait for your shipment.
-                        </p>
-                        <div className={style.time}>
-                            <Icon>󱎫</Icon> <span>1/28/2026</span> <span>18.30</span>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-            
-
-            <div className={style.order}>
-                <div className={style.productImg}>
-                    <img src="https://placehold.co/400" alt="" />
-                </div>
-
-                <div className={style.productInfo}>
-                    <h3>Order Cancelled</h3>
-                    
-                    <div className={style.productBottom}>
-                        <p>
-                            Order <Link href="#">Order number</Link> has been cancelled as 
-                            your payment could not be verified.
-                        </p>
-                        <div className={style.time}>
-                            <Icon>󱎫</Icon> <span>1/18/2026</span> <span>01.30</span>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+            {orders.length === 0 ? (
+                <section>
+                    <p>Youdont have any updates yet</p>
+                </section>
+            ) : (
+                orders.map((item , index) => (
+                    <NotificationComponent
+                    key={index}
+                    linkToPage={item.linkToPage}
+                    title = {item.title}
+                    description={item.description}
+                    mainImage={item.mainImage}
+                    hasButton = {hasButton}
+                    timestamp={item.timestamp}
+                    />
+                ))
+            )}
 
         </section>
     );
