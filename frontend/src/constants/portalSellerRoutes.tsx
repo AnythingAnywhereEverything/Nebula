c9c7356e09a451fa521d9fa34de6cfe1b27c842c
seinaru <krittitee.petch@gmail.com>
2026-02-10 00:08:51 +0700

    feat: add /myProduct

--- frontend/src/constants/portalSellerRoutes.tsx
+++ frontend/src/constants/portalSellerRoutes.tsx
@@ -1,7 +1,12 @@
+import MyProduct from "@components/features/seller/myProduct/myProduct";
+
 export const portalSellerAllowedList: Record<string, {
     component: React.ComponentType;
 }> = {
     "/dashboard": {
         component: () => <p>Dashboard</p>
+    },
+    "/myProduct": {
+        component: MyProduct
     }
 }
