177566fd1ab7149386db9f97ad82b3ca8a9e9d6d
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-02-13 14:54:13 +0700

    update(snowflake): add custom epoch date set to 1st jan 2026

--- backend/rust-service/src/application/service/snowflake_service.rs
+++ backend/rust-service/src/application/service/snowflake_service.rs
@@ -1,16 +1,15 @@
 use std::sync::Mutex;
-use std::time::{SystemTime, SystemTimeError, UNIX_EPOCH};
+use std::time::{SystemTime, UNIX_EPOCH};
 use thiserror::Error;
 
-// Constants for bit allocation (adjust as needed)
-// const TIMESTAMP_BITS: u64 = 41;
+const CUSTOM_EPOCH: u64 = 1767225600000;
+
 const WORKER_ID_BITS: u64 = 10;
 const SEQUENCE_BITS: u64 = 12;
 
 const MAX_WORKER_ID: u64 = (1 << WORKER_ID_BITS) - 1;
 const MAX_SEQUENCE: u64 = (1 << SEQUENCE_BITS) - 1;
 
-// Shift amounts
 const WORKER_ID_SHIFT: u64 = SEQUENCE_BITS;
 const TIMESTAMP_SHIFT: u64 = SEQUENCE_BITS + WORKER_ID_BITS;
 
@@ -18,20 +17,19 @@ const TIMESTAMP_SHIFT: u64 = SEQUENCE_BITS + WORKER_ID_BITS;
 pub enum SnowflakeError {
     #[error("Worker ID ({0}) exceeds maximum allowed value ({1}).")]
     InvalidWorkerId(u64, u64),
-    #[error("Sequence overflow, need to wait for next millisecond.")]
-    SequenceOverflow,
-    #[error("Clock moved backward, refusing to generate ID.")]
+
+    #[error("Clock moved backward.")]
     ClockMoveBackward,
-    #[error("Failed to acquire mutex lock: {0}")]
-    MutexLockError(String),
-    #[error("System time error: {0}")]
-    SystemTimeError(#[from] SystemTimeError),
+}
+
+struct State {
+    last_timestamp: u64,
+    sequence: u64,
 }
 
 pub struct SnowflakeGenerator {
     worker_id: u64,
-    last_timestamp: Mutex<u64>,
-    sequence: Mutex<u64>,
+    state: Mutex<State>,
 }
 
 impl SnowflakeGenerator {
@@ -40,46 +38,51 @@ impl SnowflakeGenerator {
             return Err(SnowflakeError::InvalidWorkerId(worker_id, MAX_WORKER_ID));
         }
 
-        Ok(SnowflakeGenerator {
+        Ok(Self {
             worker_id,
-            last_timestamp: Mutex::new(0),
-            sequence: Mutex::new(0),
+            state: Mutex::new(State {
+                last_timestamp: 0,
+                sequence: 0,
+            }),
         })
     }
 
     pub fn generate_id(&self) -> Result<i64, SnowflakeError> {
-        let mut last_timestamp_guard = self
-            .last_timestamp
-            .lock()
-            .map_err(|e| SnowflakeError::MutexLockError(format!("{}", e)))?; // Convert PoisonError to String
-        let mut sequence_guard = self
-            .sequence
-            .lock()
-            .map_err(|e| SnowflakeError::MutexLockError(format!("{}", e)))?; // Convert PoisonError to String
+        let mut state = self.state.lock().unwrap();
 
-        let current_timestamp = SystemTime::now()
-            .duration_since(UNIX_EPOCH)? // This will automatically convert SystemTimeError due to #[from]
-            .as_millis() as u64;
+        let mut current_timestamp = current_time_millis()?;
 
-        if current_timestamp < *last_timestamp_guard {
+        if current_timestamp < state.last_timestamp {
             return Err(SnowflakeError::ClockMoveBackward);
         }
 
-        if current_timestamp == *last_timestamp_guard {
-            *sequence_guard = (*sequence_guard + 1) & MAX_SEQUENCE;
-            if *sequence_guard == 0 {
-                return Err(SnowflakeError::SequenceOverflow);
+        if current_timestamp == state.last_timestamp {
+            state.sequence = (state.sequence + 1) & MAX_SEQUENCE;
+
+            if state.sequence == 0 {
+                while current_timestamp <= state.last_timestamp {
+                    current_timestamp = current_time_millis()?;
+                }
+                state.sequence = 0;
             }
         } else {
-            *sequence_guard = 0;
+            state.sequence = 0;
         }
 
-        *last_timestamp_guard = current_timestamp;
+        state.last_timestamp = current_timestamp;
 
         let id = (current_timestamp << TIMESTAMP_SHIFT)
             | (self.worker_id << WORKER_ID_SHIFT)
-            | *sequence_guard;
+            | state.sequence;
 
         Ok(id as i64)
     }
+}
+
+fn current_time_millis() -> Result<u64, SnowflakeError> {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .map_err(|_| SnowflakeError::ClockMoveBackward)?
+        .as_millis() as u64;
+    Ok(now - CUSTOM_EPOCH)
 }
