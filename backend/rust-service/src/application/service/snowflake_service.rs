d500d82dade2775e21440f0f6ff954a1a3761fea
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-02-15 05:18:23 +0700

    update(snowflake): make snowflake able to handle tasks up to 4

--- backend/rust-service/src/application/service/snowflake_service.rs
+++ backend/rust-service/src/application/service/snowflake_service.rs
@@ -1,17 +1,22 @@
 use std::sync::Mutex;
 use std::time::{SystemTime, UNIX_EPOCH};
+use hyper::StatusCode;
 use thiserror::Error;
+
+use crate::api::{APIError, APIErrorCode, APIErrorEntry, APIErrorKind};
 
 const CUSTOM_EPOCH: u64 = 1767225600000;
 
 const WORKER_ID_BITS: u64 = 10;
-const SEQUENCE_BITS: u64 = 12;
+const KIND_BITS: u64 = 2;
+const SEQUENCE_BITS: u64 = 10;
 
 const MAX_WORKER_ID: u64 = (1 << WORKER_ID_BITS) - 1;
 const MAX_SEQUENCE: u64 = (1 << SEQUENCE_BITS) - 1;
 
-const WORKER_ID_SHIFT: u64 = SEQUENCE_BITS;
-const TIMESTAMP_SHIFT: u64 = SEQUENCE_BITS + WORKER_ID_BITS;
+const KIND_SHIFT: u64 = SEQUENCE_BITS;
+const WORKER_ID_SHIFT: u64 = SEQUENCE_BITS + KIND_BITS;
+const TIMESTAMP_SHIFT: u64 = SEQUENCE_BITS + KIND_BITS + WORKER_ID_BITS;
 
 #[derive(Debug, Error)]
 pub enum SnowflakeError {
@@ -27,19 +32,28 @@ struct State {
     sequence: u64,
 }
 
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum SnowflakeKind {
+    Api = 0,
+    Image = 1,
+}
+
 pub struct SnowflakeGenerator {
     worker_id: u64,
+    kind: SnowflakeKind,
     state: Mutex<State>,
 }
 
 impl SnowflakeGenerator {
-    pub fn new(worker_id: u64) -> Result<Self, SnowflakeError> {
+    pub fn new(worker_id: u64, kind: SnowflakeKind) -> Result<Self, SnowflakeError> {
         if worker_id > MAX_WORKER_ID {
             return Err(SnowflakeError::InvalidWorkerId(worker_id, MAX_WORKER_ID));
         }
 
         Ok(Self {
             worker_id,
+            kind,
             state: Mutex::new(State {
                 last_timestamp: 0,
                 sequence: 0,
@@ -49,7 +63,6 @@ impl SnowflakeGenerator {
 
     pub fn generate_id(&self) -> Result<i64, SnowflakeError> {
         let mut state = self.state.lock().unwrap();
-
         let mut current_timestamp = current_time_millis()?;
 
         if current_timestamp < state.last_timestamp {
@@ -73,6 +86,7 @@ impl SnowflakeGenerator {
 
         let id = (current_timestamp << TIMESTAMP_SHIFT)
             | (self.worker_id << WORKER_ID_SHIFT)
+            | ((self.kind as u64) << KIND_SHIFT)
             | state.sequence;
 
         Ok(id as i64)
@@ -85,4 +99,28 @@ fn current_time_millis() -> Result<u64, SnowflakeError> {
         .map_err(|_| SnowflakeError::ClockMoveBackward)?
         .as_millis() as u64;
     Ok(now - CUSTOM_EPOCH)
+}
+
+impl From<SnowflakeError> for APIError {
+    fn from(snowflake_error: SnowflakeError) -> Self {
+        let (status_code, code) = match snowflake_error {
+            SnowflakeError::InvalidWorkerId(_, _) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                APIErrorCode::SnowflakeError,
+            ),
+            SnowflakeError::ClockMoveBackward => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                APIErrorCode::SnowflakeError,
+            )
+        };
+
+        let error = APIErrorEntry::new(&snowflake_error.to_string())
+            .code(code)
+            .kind(APIErrorKind::SnowflakeError);
+
+        Self {
+            status: status_code.as_u16(),
+            errors: vec![error],
+        }
+    }
 }
