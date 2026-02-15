df6807a9e9592e61dc9a7f020b37c693f84ec1dd
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-02-15 17:57:43 +0700

    fix(docker): fix image unable to load using next image

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,21 +10,31 @@ const nextConfig = {
 
   images: {
     remotePatterns: [
+      // {
+      //   protocol: "http",
+      //   hostname: "localhost",
+      //   port: "",
+      //   pathname: "/cdn/**",
+      // },
       {
         protocol: "http",
-        hostname: "localhost",
+        hostname: "nginx",
         port: "",
         pathname: "/cdn/**",
       },
     ],
-    dangerouslyAllowLocalIP: true, // NOTE: For development only
+    // dangerouslyAllowLocalIP: true, // NOTE: For development only
   },
 
   async rewrites() {
     return [
+      // {
+      //   source: "/cdn/:path*",
+      //   destination: "http://localhost/cdn/:path*",
+      // },
       {
         source: "/cdn/:path*",
-        destination: "http://localhost/cdn/:path*",
+        destination: "http://nginx/cdn/:path*",
       },
     ];
   },
