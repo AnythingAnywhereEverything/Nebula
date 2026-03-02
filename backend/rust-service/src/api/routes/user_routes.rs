f26f5585e4c4a359882683433c1db6f328232aa2
seinaru <82324764+K-Hongkaew@users.noreply.github.com>
2026-03-03 00:33:09 +0700

    feat: route for update password

--- backend/rust-service/src/api/routes/user_routes.rs
+++ backend/rust-service/src/api/routes/user_routes.rs
@@ -8,7 +8,7 @@ use crate::{
         handlers::{
             session_handler::{delete_session_user_handler, get_all_session_handler},
             user_handlers::{
-                add_or_update_profile_handler, add_user_address_handler, change_display_name, change_username, delete_user_address_handler, get_user_addresses_handler, get_user_handler, set_default_address_handler, update_user_address_handler
+                add_or_update_profile_handler, add_user_address_handler, change_display_name, change_username, delete_user_address_handler, get_user_addresses_handler, get_user_handler, set_default_address_handler, set_new_password_handler, update_user_address_handler
             },
         },
         middleware::user_mw,
@@ -31,7 +31,7 @@ pub fn routes(state: SharedState) -> Router<SharedState> {
         .route("/{id}/addresses/{address_id}",put(update_user_address_handler))
         .route("/{id}/addresses/{address_id}/default",patch(set_default_address_handler),)
         .route("/{id}/addresses/{address_id}",delete(delete_user_address_handler))
-
+        .route("/{id}/password", patch(set_new_password_handler))
         .layer(middleware::from_fn_with_state(
             state.clone(),
             user_mw::validate_user,
