ea1d0efa35f8d5c83f47cb621fb7bf8547e3305d
seinaru <82324764+K-Hongkaew@users.noreply.github.com>
2026-03-06 21:31:05 +0700

    feat: add shop route

--- backend/rust-service/src/api/routes/shop_routes.rs
+++ backend/rust-service/src/api/routes/shop_routes.rs
@@ -1,14 +1,17 @@
-use axum::{Router, middleware, routing::{get, post}};
+use axum::{Router, middleware, routing::{get, patch, post, put}};
 
 use crate::{
-    api::{handlers::shop_handlers::{create_shop_handler, get_assosiate_shops_handler}, middleware::user_mw}, application::state::SharedState
+    api::{handlers::shop_handlers::{create_shop_handler, get_assosiate_shops_handler, get_current_shop_handler, update_shop_banner_handler, update_shop_info_handler, update_shop_profile_handler}, middleware::user_mw}, application::state::SharedState
 };
 
 pub fn routes(state: SharedState) -> Router<SharedState> {
     Router::new()
         .route("/create", post(create_shop_handler))
         .route("/{id}", get(get_assosiate_shops_handler))
-        
+        .route("/{id}/info", get(get_current_shop_handler))
+        .route("/{id}/profile", patch(update_shop_profile_handler))
+        .route("/{id}/banner", patch(update_shop_banner_handler))
+        .route("/{id}/info", put(update_shop_info_handler))
         .layer(middleware::from_fn_with_state(
             state.clone(),
             user_mw::validate_user,
