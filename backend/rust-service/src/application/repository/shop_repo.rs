4162d7271090e81c29c7fcd84b6a3b34e9b83625
seinaru <82324764+K-Hongkaew@users.noreply.github.com>
2026-03-06 21:27:12 +0700

    feat(repo): update shop
    -Shop
    - update info
    - update profile
    - update banner

--- backend/rust-service/src/application/repository/shop_repo.rs
+++ backend/rust-service/src/application/repository/shop_repo.rs
@@ -1,6 +1,6 @@
 use sqlx::{Postgres, Transaction};
 
-use crate::{application::repository::errors::ShopRepoError, domain::models::shop::{NewShop, Shop}};
+use crate::{application::repository::errors::ShopRepoError, domain::models::shop::{NewShop, Shop, ShopUpdateData}};
 
 pub async fn is_shop_exist (
     tx: &mut Transaction<'_, Postgres>,
@@ -43,7 +43,7 @@ pub async fn get_shop_by_owner_id(
 ) -> Result<Vec<Shop>, ShopRepoError> {
     let shops = sqlx::query_as::<_, Shop>(
         r#"
-        SELECT id, name, description, owner_id, is_brand, created_at, updated_at
+        SELECT id, name, description, owner_id, is_brand, created_at, updated_at, shop_profile_url, shop_banner_url
         FROM shops
         WHERE owner_id = $1
         "#,
@@ -51,7 +51,7 @@ pub async fn get_shop_by_owner_id(
     .bind(owner_id)
     .fetch_all(tx.as_mut())
     .await
-    .map_err(|_| ShopRepoError::FailedToGetShop)?;
+    .map_err(|_| ShopRepoError::ShopNotFound)?;
     Ok(shops)
 }
 
@@ -61,7 +61,7 @@ pub async fn get_shop_by_member_id(
 ) -> Result<Vec<Shop>, ShopRepoError> {
     let shops = sqlx::query_as::<_, Shop>(
         r#"
-        SELECT s.id, s.name, s.description, s.owner_id, s.is_brand, s.created_at, s.updated_at
+        SELECT s.id, s.name, s.description, s.owner_id, s.is_brand, s.created_at, s.updated_at, shop_profile_url, shop_banner_url
         FROM shops s
         JOIN shop_members sm ON s.id = sm.shop_id
         WHERE sm.user_id = $1
@@ -70,6 +70,98 @@ pub async fn get_shop_by_member_id(
     .bind(user_id)
     .fetch_all(tx.as_mut())
     .await
-    .map_err(|_| ShopRepoError::FailedToGetShop)?;
+    .map_err(|_| ShopRepoError::ShopNotFound)?;
     Ok(shops)
+}
+// * router query
+pub async fn get_current_shop_by_shop_id(
+    tx: &mut Transaction<'_, Postgres>,
+    shop_id: i64,
+) -> Result<Shop, ShopRepoError >{
+    let shop = sqlx::query_as::<_, Shop>(
+        r#"
+        SELECT *
+        FROM shops
+        WHERE id = $1
+        "#
+    )
+    .bind(shop_id)
+    .fetch_one(tx.as_mut())
+    .await?;
+
+    Ok(shop)
+}
+
+pub async fn update_info_shop(
+    tx: &mut Transaction<'_, Postgres>,
+    id: i64,
+    shop: ShopUpdateData
+)-> Result<(),ShopRepoError> {
+    let _ = sqlx::query(
+        r#"
+        UPDATE shops
+        SET name = $1,
+            description = $2,
+            updated_at = $3
+        WHERE id = $4
+        "#
+    )
+    .bind(shop.name)
+    .bind(shop.description)
+    .bind(chrono::Utc::now().naive_utc())
+    .bind(id)
+    .execute(tx.as_mut())
+    .await
+    .map_err(|e| match e {
+        sqlx::Error::RowNotFound => ShopRepoError::ShopNotFound,
+        _ => ShopRepoError::from(e),
+    })?;
+
+    Ok(())
+}
+
+pub async fn update_profile_shop(
+    tx: &mut Transaction<'_, Postgres>,
+    id: i64,
+    shop: Shop,
+)-> Result<(),ShopRepoError> {
+    let _ = sqlx::query(
+        r#"
+        UPDATE shops
+        SET shop_profile_url = $1,
+        updated_at = $2
+        WHERE id = $3
+        "#
+    )
+    .bind(shop.shop_profile_url)
+    .bind(chrono::Utc::now().naive_utc())
+    .bind(id)
+    .execute(tx.as_mut())
+    .await
+    .map_err(|_| ShopRepoError::FailedToUpdateShop);
+
+    Ok(())
+}
+
+pub async fn update_banner_shop(
+    tx: &mut Transaction<'_, Postgres>,
+    id: i64,
+    shop: Shop,
+)-> Result<(),ShopRepoError> {
+    let _ = sqlx::query(
+        r#"
+        UPDATE shops
+        SET shop_banner_url = $1,
+        updated_at = $2
+        WHERE id = $3
+        "#
+    )
+    .bind(shop.shop_banner_url)
+    .bind(chrono::Utc::now().naive_utc())
+    .bind(id)
+    .execute(tx.as_mut())
+    .await
+    .map(|_| ShopRepoError::ShopNotFound);
+
+    Ok(())
 }
